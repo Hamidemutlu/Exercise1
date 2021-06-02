f58d1156949de349e69e66895076b08248351e86
Hamidemutlu <71645913+Hamidemutlu@users.noreply.github.com>
2021-06-02 13:12:36 +0200

    length Test written

--- src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
+++ src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
@@ -1,4 +1,15 @@
 package at.ac.fhcampuswien;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PasswordValidatorTest {
+    @Test
+    @DisplayName("")
+    public void lengthTest(){
+        PasswordValidator p = new PasswordValidator();
+        boolean actual = p.length();
+        assertTrue(actual);
+    }
 }
