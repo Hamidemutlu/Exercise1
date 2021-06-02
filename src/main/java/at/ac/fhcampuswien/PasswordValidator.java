f74e4d5d2a537dd9b9b9e334210bfceca7d539ba
Hamidemutlu <71645913+Hamidemutlu@users.noreply.github.com>
2021-06-02 14:44:46 +0200

    letter Test written

--- src/main/java/at/ac/fhcampuswien/PasswordValidator.java
+++ src/main/java/at/ac/fhcampuswien/PasswordValidator.java
@@ -1,4 +1,7 @@
 package at.ac.fhcampuswien;
 
 public class PasswordValidator {
+    public boolean length(String password){
+        return password.length() >= 8 && password.length() <= 25;
+    }
 }
--- src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
+++ src/test/java/at/ac/fhcampuswien/PasswordValidatorTest.java
@@ -7,9 +7,33 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordValidatorTest {
     @Test
     @DisplayName("")
-    public void lengthTest(){
+    public void lengthTest1(){
         PasswordValidator p = new PasswordValidator();
-        boolean actual = p.length();
+        boolean actual = p.length("124530480986Ba?30324809");
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("")
+    public void lengthTest2(){
+        PasswordValidator p = new PasswordValidator();
+        boolean actual = p.length("12Ba3?");
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("")
+    public void lengthTest3(){
+        PasswordValidator p = new PasswordValidator();
+        boolean actual = p.length("124530480986094303248093667");
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("")
+    public void letter1(){
+        PasswordValidator p = new PasswordValidator();
+        boolean actual = p.letter("124530480986Ba?30324809");
         assertTrue(actual);
     }
 }
